239ca8acb51b6c18d90e7b1dc46c2b62a4f955f4
Hritik Shukla <hritikshuklas@gmail.com>
2023-12-03 23:58:35 -0500

    fixed some edge cases for parse, added semi-functional export

--- src/midi_parser.py
+++ src/midi_parser.py
@@ -1,17 +1,21 @@
 from mido import Message, MetaMessage, MidiFile, MidiTrack, bpm2tempo, second2tick
-
+import os
 
 class Note:
     def __init__(self, note, velocity, offset):
         self.note = note
         self.velocity = velocity
         self.offset = offset
+        self.duration = None
 
     def set_duration(self, duration):
         self.duration = duration
     
     def __str__(self) -> str:
-        return "Note: " +  str(self.note) + " Velocity: " + str(self.velocity) + " Offset: " + str(self.offset) + " Duration: " + str(self.duration )
+        return "(Note: " +  str(self.note) + " Velocity: " + str(self.velocity) + " Offset: " + str(self.offset) + " Duration: " + str(self.duration ) + ")"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
 
 
 class Midi:
@@ -24,11 +28,16 @@ class Midi:
         track = self.midi.tracks[1]
         curr_duration = 0
         sequence = []
-
+        offset_compensation = 0
         for i in range(len(track)):
             curr_duration = 0
+            
+            if track[i].type == 'control_change' or track[i].type == 'note_off' or (track[i].type == 'note_on' and track[i].velocity == 0):
+                offset_compensation += track[i].time
+
             if track[i].type == 'note_on' and track[i].velocity != 0:
-                seq_note = Note(track[i].note, track[i].velocity, track[i].time)
+                seq_note = Note(track[i].note, track[i].velocity, track[i].time + offset_compensation)
+                offset_compensation = 0
                 sequence.append(seq_note)
 
                 for j in range(i + 1, len(track)):
@@ -36,18 +45,52 @@ class Midi:
                     curr_duration += curr_note.time
 
                     if (curr_note.type == 'note_off' or (curr_note.type == 'note_on' and curr_note.velocity == 0)) and curr_note.note == seq_note.note:
+                        # seq_note.offset += curr_note.time
                         seq_note.set_duration(curr_duration)
                         break
+        self.notes = sequence.copy()
     
     def export(self, path):
-        pass
+        # TODO: Figure out time
+        midi = MidiFile()
+        track = MidiTrack()
+        midi.tracks.append(track)
+
+        notes = self.notes.copy()
+        for i  in range(len(notes)):
+            # Look at current note
+            curr_note = notes[i]
+            curr_offset = curr_note.offset
+            # End any notes before current note
+            for j in range(0, i):
+                # Look at previous notes
+                prev_note = notes[j]
+                # If note at index == None, already done playing the note
+                if prev_note == None:
+                    continue
+                # Subtract current offset from their duration
+                if prev_note.duration - curr_offset <= 0:
+                    # End the note
+                    track.append(Message('note_on', note=prev_note.note, velocity=0, time=curr_offset-prev_note.duration))
+                    # Set the note as played
+                    notes[j] = None
+                else:
+                    prev_note.duration -= curr_offset
+            # Add current note
+            track.append(Message('note_on', note=curr_note.note, velocity=curr_note.velocity, time=curr_note.offset))
+            
+        track.append(MetaMessage('end_of_track', time=1))
+        # print(track)
+        midi.save(path)
+
+
 
     def add_note(self, note: Note):
         pass
 
 
 
-# mid = MidiFile('../data/midis/Turchetto, Andrea, Variations on a Theme by Mozart, QihjMKKNdo0.mid', clip=True)
+# mid = MidiFile('../data/midis/Beethoven, Ludwig van, Für Elise, WoO 59, noAU3qDS1dA.mid', clip=True)
 # print(mid.tracks[1])
 # track = mid.tracks[1]
 # track.pop()
@@ -55,3 +98,9 @@ class Midi:
 # track.append(MetaMessage('end_of_track'))
 # print(mid.tracks[1])
 # mid.save('new_song.mid')
+
+mid = Midi('../data/midis/Beethoven, Ludwig van, Für Elise, WoO 59, noAU3qDS1dA.mid')
+mid.parse()
+# for note in mid.notes:
+#     print(note)
+mid.export("export_test.mid")
