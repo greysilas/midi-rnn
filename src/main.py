b3a1a666f9ef987134ac673011b94787dd537a11
Hritik Shukla <hritikshuklas@gmail.com>
2023-12-11 01:00:20 -0500

    luh calm commit nun too extravagant

--- src/main.py
+++ src/main.py
@@ -17,45 +17,6 @@ SEQUENCE_LENGTH = 200
 BATCH_SIZE = 20
 MIDI_PATH = "../data/midis/"
 
-def pad_sequence(text_list, padding_value):
-    pass
-
-def collate_batch(batch):
-    """
-    Returns the input and target tensors for a batch of data
-
-    Parameters:
-        `batch` - An iterable data structure of tuples (indices, label),
-                  where `indices` is a sequence of word indices, and
-                  `label` is either 1 or 0.
-
-    Returns: a tuple `(X, t)`, where
-        - `X` is a PyTorch tensor of shape (batch_size, sequence_length)
-        - `t` is a PyTorch tensor of shape (batch_size)
-    where `sequence_length` is the length of the longest sequence in the batch
-    """
-    text_list = []  # collect each sample's sequence of word indices
-    label_list = [] # collect each sample's target labels
-
-    for (note_list, label) in batch:
-        text_list.append(torch.tensor(text_indices))
-        # TODO: what do we need to do with `label`?
-        label_list.append(label)
-        
-    # song
-    # seq_len = 20 <-  
-    # for i in range(len(song)-seq_len + 1):
-    #   X = song[i:seq_len]
-    #   t = song[seq_len]
-    #   i++
-    #
-    
-    X = pad_sequence(text_list, padding_value=3).transpose(0, 1)
-    t = torch.tensor(label_list) # TODO
-    return X, t
-
-
-
 def get_batch(data, block_size, batch_size, device):
     """
     Return a minibatch of data. This function is not deterministic.
@@ -75,14 +36,16 @@ def get_batch(data, block_size, batch_size, device):
     # print("get_batch")
     # print("batch size", batch_size)
     # print("data len", len(data))
-    block_size = min(len(data), block_size)
+    block_size = min(data.shape[0], block_size)
     # print("block size", block_size)
-    ix = torch.randint(len(data) - block_size, (batch_size,))
+    ix = torch.randint(data.shape[0] - block_size, (batch_size,))
     # print(ix)
-    x = torch.stack([torch.tensor((data[i:i+block_size])) for i in ix])
+    # x = torch.stack([torch.tensor((data[i:i+block_size])).unsqueeze(-1) for i in ix])
+    x = torch.stack([(data[i:i+block_size]) for i in ix])
     # t = torch.stack([torch.tensor((data[i+1:i+1+block_size])) for i in ix])
     # OR
-    t = torch.stack([torch.tensor((data[i+block_size])) for i in ix])
+    t = torch.stack([(data[i+block_size]) for i in ix])
+ 
     # WHICH ONE?????????????????????? SECOND ONE
     # THANKS. WELCOME.
     if 'cuda' in device:
@@ -92,7 +55,6 @@ def get_batch(data, block_size, batch_size, device):
     else:
         x, t = x.to(device), t.to(device)
     return x, t
-
 
 
 if __name__ == '__main__':
@@ -108,42 +70,44 @@ if __name__ == '__main__':
     valid_ratio=0.2
 
     print("Processing MIDIS")
-    files = [f for f in os.listdir(MIDI_PATH) if os.path.isfile(os.path.join(MIDI_PATH, f))]
-    
-    for filename in files:
-        # print(filename)
-        if filename.endswith(".midi") or filename.endswith(".mid"): 
-            path = os.path.join(MIDI_PATH, filename) # if below breaks
-            curr_midi = Midi(path)
-            curr_midi.parse()
-            formatted_song = []
-            for note in curr_midi.notes:
-                formatted_song.append((note.note, note.velocity, note.offset_norm, note.duration_norm))    
-            formatted_songs.append(formatted_song)
-            formatted_notes.extend(formatted_song)
-            total_songs += 1
+    # files = [f for f in os.listdir(MIDI_PATH) if os.path.isfile(os.path.join(MIDI_PATH, f))]
+    # random.shuffle(files)
+
+    # for filename in files:
+    #     # print(filename)
+    #     if filename.endswith(".midi") or filename.endswith(".mid"): 
+    #         path = os.path.join(MIDI_PATH, filename) # if below breaks
+    #         curr_midi = Midi(path)
+    #         curr_midi.parse()
+    #         formatted_song = []
+    #         for note in curr_midi.notes:
+    #             formatted_song.append((note.note, note.velocity, note.offset_norm, note.duration_norm))    
+    #         formatted_songs.append(formatted_song)
+    #         formatted_notes.extend(formatted_song)
+    #         total_songs += 1
+    #         if total_songs == 2500:
+    #             break
        
-        else:
-            continue
+    #     else:
+    #         continue
 
 
-    # Serialize the songs
-    with open('../data/serialized/songs10854.pkl', 'wb') as f:  # open a text file
-        pickle.dump(formatted_songs, f) # serialize the list
-    with open('../data/serialized/notes10854.pkl', 'wb') as f:  # open a text file
-        pickle.dump(formatted_notes, f) # serialize the list
+    # # Serialize the songs
+    # with open('../data/serialized/songs2500.pkl', 'wb') as f:  # open a text file
+    #     pickle.dump(formatted_songs, f) # serialize the list
+    # with open('../data/serialized/notes2500.pkl', 'wb') as f:  # open a text file
+    #     pickle.dump(formatted_notes, f) # serialize the list
     
  
-    exit()
+    # exit()
    
-    with open('../data/serialized/songs500.pkl', 'rb') as f:  # open a text file
-        formatted_songs = pickle.load(f) # serialize the list
+    print("Loading data...")
+    # with open('../data/serialized/songs2500.pkl', 'rb') as f:  # open a text file
+    #     formatted_songs = pickle.load(f) # serialize the list
     with open('../data/serialized/notes500.pkl', 'rb') as f:  # open a text file
         formatted_notes = pickle.load(f) # serialize the list
-    
-    
-    print("Songs:", formatted_songs[0])
-    print("Notes:", formatted_notes[0])
+    print("Data loaded.")
+ 
 
 
     # print("Formatted Notes:", len(formatted_notes))
@@ -158,9 +122,14 @@ if __name__ == '__main__':
 
     train_data = formatted_notes[:floor(len(formatted_notes)*train_ratio)] 
     val_data = formatted_notes[floor(len(formatted_notes)*train_ratio):]
- 
+    # print("Train data len:" ,len(train_data))
+    train_data = [a[0] for a in train_data]
+    val_data = [a[0] for a in val_data]
+    # train_data = [(float(a[0]),) for a in train_data]
+    # val_data = [(float(a[0]),) for a in val_data]
+    # print(train_data[-5:])
     #dataset= torch.utils.data.MyIterableDataset(formatted_songs)
-    
+    # exit()
     # Concatenate all songs in training set
     # Create model instance
 
@@ -169,29 +138,49 @@ if __name__ == '__main__':
     #print(len(formatted_songs[0]))
     #print(formatted_songs)
     device = 'cuda' if torch.cuda.is_available()  else 'cpu'
-  
+    # device = 'cpu'
+    
+
+    # print(train_data[:10])
+    train_data = torch.tensor(train_data)
+    val_data = torch.tensor(val_data)
+    
+    train_one_hot = torch.nn.functional.one_hot(train_data, num_classes=128).float()
+    val_one_hot = torch.nn.functional.one_hot(val_data, num_classes=128).float()
+    # # Calculate the mean and standard deviation of each feature in the training set
+    # X_mean = train_data.mean(dim=0)
+    # X_std = train_data.std(dim=0)
+
+    # # Standardize the training set
+    # train_data = (train_data - X_mean) / X_std
+
+    # print(train_data.shape)
+
+    # # Standardize the test set using the mean and standard deviation of the training set
+    # val_data = (val_data - X_mean) / X_std
+    
+    # exit()
+ 
   
     # train_dataloader = torch.utils.data.DataLoader(formatted_songs, batch_size=10, shuffle=True,
     #                           collate_fn=collate_batch)
-    print(torch.cuda.get_device_name())
-    mod = model.midiRNN(4, 256, 4)
+    # print(torch.cuda.get_device_name())
+    mod = model.midiRNN(128, 256, 128, device=device)
     mod = mod.to(device)
-    # mod.train()
-    # model.train_model(mod, train_data, val_data, num_epochs=3000, batch_size=128, block_size=256,
-    #                    plot_every=250, device=device)
-    # torch.save(mod.state_dict(), './500songshidden256epoch3000bat128block256')
-    
-    
-    # mod.load_state_dict(torch.load('./500songshidden256epoch3000bat128block256'))
-    # mod.eval()
-    # torch.no_grad()
-    # model.generate_song(mod, val_data, device=device)
+    # train_data = train_data.to(device)
+    # val_data = val_data.to(device)
+    trainModel = False
+    model_state_name = './2500songshidden256epoch5bat128block256'
+    if trainModel:
+        mod.train()
+        model.train_model(mod, train_one_hot, val_one_hot, num_epochs=1, batch_size=128, block_size=512,
+                        plot_every=500, device=device)
+        torch.save(mod.state_dict(), model_state_name)
+    else:
+        mod.load_state_dict(torch.load(model_state_name))
+        mod.eval()
+        torch.no_grad()
+        model.generate_song(mod, val_one_hot, device=device)
     
 
-    # data = sequence of all notes
-    #   - training
-    #   - seeds
-    # 
-    # generation
-    #   - get_batch(seed) in seed, extract a random batch
-    #   - x number of notes to start, then generate after
+
--- src/model.py
+++ src/model.py
@@ -18,103 +18,113 @@ class midiRNN(nn.Module):
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
-        self.rnn = nn.RNN(input_size, 2*hidden_size, batch_first=True)
-        self.fc1 = nn.Linear(2*hidden_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, 4) 
-        # self.activation = nn.Tanh()
-
-    def forward(self, X):
-        # print(X.shape)
-        out, h = self.rnn(X) # (block_size, hidden_size)
+        # self.fc0 = nn.Linear(input_size, hidden_size)
+        self.rnn = nn.LSTM(input_size, hidden_size, batch_first=True)
+        self.fc1 = nn.Linear(hidden_size, output_size)
+        # self.fc2 = nn.Linear(hidden_size,output_size ) 
       
+    def forward(self, X):
+        # print("X shape before fc0:", X.shape)
+        # out = self.fc0(X.float())
+        # print("X shape", X.shape)
+        out, h = self.rnn(X.float(),) # (block_size, hidden_size)
+
         # print("OUT shape:", out.shape)
         # features = torch.cat([torch.amax(h, dim=1),
         #                       torch.mean(h, dim=1)], axis=-1)
-        out = F.relu(self.fc1(out[:, -1, :])) # Gets the last timestep
-        out = F.relu(self.fc2(out))
+        out = self.fc1(out[:, -1, :]) # Gets the last timestep
+       # out = F.softmax(out)
+
+        # print("Out prob dist: ", out)
+        # print("Highest prob: ", torch.max(out))
 
         # print("FORWARD Z shape:",z.shape)
         # y = self.activation(z) #(hidden_size, output_size)
         # print("Y shape:",y.shape)
         return out
 
-def accuracy(model, dataset, max=1000, device='cpu'):
-    correct, total = 0, 0
-    # dataloader = DataLoader(dataset,
-    #                         batch_size=1,  # use batch size 1 to prevent padding
-    #                         collate_fn=collate_batch)
-    #for i, (x, t) in enumerate(dataloader):
-    #     z = model(x)
-    #     y = torch.argmax(z, axis=)
-    # for each song in dataset:
-    #   for i in range(num_batches): same number as hyperparameter set in main.py
-    #       get random batch (get_batch), 
-    #           Get prediction, +1 if correct
-    #
-    # print("IN ACCURACY")
-    # print("Validation len", len(dataset))
-    for i in range(BATCH_SIZE):
-        xs, ts = get_batch(dataset, batch_size=100, block_size=256,device=device)
-        y = model(xs)
-        o = torch.eq(y, ts)
-        # print("Y shape", y.shape)
-        # print("Ts shape", ts.shape)
-        # print(y[:2])
-        # print(ts[:2])
-        if False not in o:
-            correct += 1
-        total +=1
-    return correct / total
-
 def train_model(model,                # an instance of MLPModel
                 train_data,           # training data
                 val_data,             # validation data
-                learning_rate=0.001,
+                learning_rate=0.0003,
                 batch_size=1,
-                block_size=256,
+                block_size=20,
                 num_epochs=500,
-                num_iters_per_score = 5,
                 plot_every=10,
                 device='cpu',
                 plot=True):
    
-    criterion = nn.MSELoss()
+
+    # Convert input to one hots
+    # train_one_hots = []
+    # for note in train_data:
+    #     x_one_hot = np.zeros(128)
+    #     x_one_hot[note ] = 1
+
+   
+
+    criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
     iters, train_loss, train_acc, val_acc = [], [], [], []
     iter_count = 0 # count the number of iterations that has passed
 
     for e in range(num_epochs):
-        x, t = get_batch(train_data, block_size=block_size, batch_size=batch_size, device=device)
-    # for i in range(xs.shape[0]):
-        # print(len(get_batch(train_data, block_size=block_size, batch_size=batch_size,device=device)[0]))
-    #     x = xs[i]
-    #     t = ts[i]  
-        z = model(x)
-        # print("Z")
-        # print(z)
-        # print("T")
-        # print(t)
-        
-        # print(z)
-        # print(ts)
-        # print("Z:", z.shape)
-        # print("Ts", t.shape)
-        loss = criterion(z, t)
+        num_batches = 2500
+        for s in range(num_batches):
+            x, t = get_batch(train_data, block_size=block_size, batch_size=batch_size, device=device)
+            
+            # print(t)
+            # for i in range(xs.shape[0]):
+                # print(len(get_batch(train_data, block_size=block_size, batch_size=batch_size,device=device)[0]))
+            #     x = xs[i]
+            #     t = ts[i]  
 
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+            z = model(x)
+            # print("Z")
+            # print(z)
+            # print("T")
+            # print(t)
+            
+            # print(z)
+            # print(ts)
+            # print("Z:", z.shape)
+            # print("Ts", t.shape)
+            pred = torch.argmax(z, dim=1).unsqueeze(1)
+            # print("Z", z)
+            # print("pred", pred)
+            # print("T", t)
+            # print("Pred shape", pred.shape)
+            # print("T Shape", t.shape)
+            # print("Pred shape", pred.shape)
+            # print("z shape", z.shape)
+            # print(z)
+            # print(t.squeeze().long())
+            # one_hot_t = torch.nn.functional.one_hot(t.squeeze().long(), num_classes=128).float()
 
-        iter_count += 1
-        if iter_count % plot_every == 0:
-            iters.append(iter_count)
-            ta = accuracy(model, train_data, device=device)
-            va = accuracy(model, val_data, device=device)
-            train_loss.append(float(loss))
-            train_acc.append(ta)
-            val_acc.append(va)
-            print(iter_count, "Loss:", float(loss), "Train Acc:", ta, "Val Acc:", va)
+            # print("one hot t shape", one_hot_t.shape)
+            # print(one_hot_t)
+            loss = criterion(z, t)
+            l1_regularization = 0
+            
+            for param in model.parameters():
+                l1_regularization += torch.norm(param, 1)**2
+              
+            loss += l1_regularization
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            iter_count += 1
+            if iter_count % plot_every == 0:
+                iters.append(iter_count)
+                # ta = accuracy(model, train_data, device=device)
+                # va = accuracy(model, val_data, device=device)
+                train_loss.append(float(loss))
+                # train_acc.append(ta)
+                # val_acc.append(va)
+                print("[" + str(iter_count) + "]:","Epoch:",e+1, "Iteration:", s+1, "Loss:", float(loss))
     if plot:
         plt.figure()
         plt.plot(iters[:len(train_loss)], train_loss)
@@ -122,25 +132,33 @@ def train_model(model,                # an instance of MLPModel
         plt.xlabel("Iterations")
         plt.ylabel("Loss")
 
-        plt.figure()
-        plt.plot(iters[:len(train_acc)], train_acc)
-        plt.plot(iters[:len(val_acc)], val_acc)
-        plt.title("Accuracy over iterations")
-        plt.xlabel("Iterations")
-        plt.ylabel("Loss")
-        plt.legend(["Train", "Validation"])
+        # plt.figure()
+        # plt.plot(iters[:len(train_acc)], train_acc)
+        # plt.plot(iters[:len(val_acc)], val_acc)
+        # plt.title("Accuracy over iterations")
+        # plt.xlabel("Iterations")
+        # plt.ylabel("Loss")
+        # plt.legend(["Train", "Validation"])
+        plt.show()
 
 def generate_song(model, seed, length=64, tempo=120, starter_size=32, device='cpu'):
     # print("Generate song")
     song, t = get_batch(seed, block_size=starter_size, batch_size=1, device=device)
-    song = torch.squeeze(song, dim=0)
+    song = song.squeeze(dim=0)
+    # song = torch.squeeze(song, dim=0)
     # print("Song tensor:", torch.is_tensor(song))
 
     # print(song.shape)
-    while len(song) < length:
-        next_note = model(torch.unsqueeze(song, dim=0))
+    while song.shape[0] < length:
+        # print("Current sequence", torch.unsqueeze(song, dim=0)[-20:])
+        next_note = model(song[-20:].unsqueeze(dim=0))
+        # torch.sum(probs,dim=1)
+        # print(song.shape)
+        # print(probs)
         # print("Note tensor:", torch.is_tensor(next_note))
         # print(next_note.shape)
+        print(next_note.shape)
+        print(song.shape)
         song = torch.cat((song, next_note), dim=0)
         # print("New song shape", song.shape)
         # Sample the next note (you might need to adjust this depending on your output space)
@@ -148,8 +166,8 @@ def generate_song(model, seed, length=64, tempo=120, starter_size=32, device='cp
         # print(song)
     # Convert to note
     midi_file = Midi()
-    print(song)
-    print(song.shape)
+    print('goofy ahh')
+    print([song])
     for note in song:
         # Create note object
         
